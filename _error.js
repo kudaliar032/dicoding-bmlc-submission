6c6a6d1f68520ad90f764579a9f5757793857726
Aditya Rahman <adityarahman032@gmail.com>
2020-12-13 20:47:40 +0800

    Integration with sentry

--- _error.js
+++ _error.js
@@ -1,10 +1,18 @@
+const Sentry = require('@sentry/node')
+
 module.exports = async function HandleError(context, props) {
-  console.error(props.error);
-  await context.sendText(`Ups, terjadi kesalahan ${String.fromCodePoint(0x10007C)}. Mohon maaf atas ketidak nyamanannya, silahkan dicoba beberapa saat lagi. Kirimkan perintah "BANTUAN" untuk mendapatkan bantuan ${String.fromCodePoint(0x10008A)}.`);
+  console.error(props.error)
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN
+  })
+
+  await context.sendText(`Ups, terjadi kesalahan ${String.fromCodePoint(0x10007C)}. Mohon maaf atas ketidak nyamanannya, silahkan dicoba beberapa saat lagi. Kirimkan perintah "BANTUAN" untuk mendapatkan bantuan ${String.fromCodePoint(0x10008A)}.`)
+
   if (process.env.NODE_ENV === 'production') {
-    // send your error to your error tracker, for example: Sentry
+    Sentry.captureException(props.error)
   }
   if (process.env.NODE_ENV === 'development') {
-    await context.sendText(props.error.stack);
+    await context.sendText(props.error.stack)
   }
-};
+}
